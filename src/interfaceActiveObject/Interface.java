7dac4e5ec63571139886788c4168a0d33538145a
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-01-26 14:59:51 +0100

    l'interface marche bien

--- src/interfaceActiveObject/Interface.java
+++ src/interfaceActiveObject/Interface.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -24,19 +25,27 @@ public class Interface extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage=primaryStage;
 		primaryStage.setTitle("ActiveObject");
-		launchInterface();
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("aoc.fxml"));
+		Scene scene = new Scene(root,800,400);
+		primaryStage.setScene(scene);
+//		FXMLLoader loader = new FXMLLoader();
+//		//loader.setLocation(Interface.class.getResource("MainView.fxml"));
+//		PanePrincipal = loader.load();
+		primaryStage.show();
 		
 		
 	} 
 	
-	public void launchInterface() throws IOException {
-		BorderPane borderPanTool = new BorderPane();
-		Scene scene = new Scene(borderPanTool,800,400);
-		primaryStage.setScene(scene);
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Interface.class.getResource("MainView.fxml"));
-		PanePrincipal = loader.load();
-		primaryStage.show();
-	}
+//	public void launchInterface() throws IOException {
+//		
+//		Parent root = FXMLLoader.load(getClass().getResource("aoc.fxml"));
+//		BorderPane borderPanTool = new BorderPane();
+//		Scene scene = new Scene(borderPanTool,800,400);
+//		primaryStage.setScene(scene);
+////		FXMLLoader loader = new FXMLLoader();
+////		//loader.setLocation(Interface.class.getResource("MainView.fxml"));
+////		PanePrincipal = loader.load();
+//		primaryStage.show();
+//	}
 
 }
--- src/interfaceActiveObject/MainViewController.java
+++ src/interfaceActiveObject/MainViewController.java
@@ -1,5 +1,46 @@
 package interfaceActiveObject;
 
-public class MainViewController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
+public class MainViewController {
+    @FXML
+    ToggleGroup broadcastMethod;
+
+    @FXML
+    Label monitor1;
+
+    @FXML
+    Label monitor2;
+
+    @FXML
+    Label monitor3;
+
+    @FXML
+    Label monitor4;
+
+    @FXML
+    RadioButton atomicButton;
+
+    @FXML
+    RadioButton causalButton;
+
+    @FXML
+    RadioButton sequentialButton;
+
+    public void initialize() {
+        
+        // listener to change broadcast method
+        this.broadcastMethod.selectedToggleProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue == this.atomicButton) {
+                // code...
+            } else if (newValue == this.causalButton) {
+                // code...
+            } else if (newValue == this.sequentialButton) {
+                // code...
+            }
+        }));
+    }
 }
